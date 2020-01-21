51133148f5c7249185ee63ed58581d88c8a32a98
Adam Fox <Adam.Fox@derivco.co.uk>
2020-01-21 13:26:43 +0000

    added stored proc to check if user exists

---
+++ WeatherRoutingDatabase/Programmable Objects/dbo/Stored Procedures/IsUserValid.sql
@@ -0,0 +1,23 @@
+IF OBJECT_ID('[dbo].[IsUserValid]') IS NOT NULL
+	DROP PROCEDURE [dbo].[IsUserValid];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [dbo].[IsUserValid] (
+	@Username VARCHAR(50),
+	@Password VARCHAR(50)
+)
+	
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 1
+	FROM [User]
+	WHERE UserId = @Username AND [Password] = @Password
+END
+GO
