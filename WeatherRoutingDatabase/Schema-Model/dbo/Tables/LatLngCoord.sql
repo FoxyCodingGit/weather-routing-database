c6994dd96e55dcf85c46509b78f0d6bbdbed487e
Adam Fox <Adam.Fox@derivco.co.uk>
2020-01-29 15:47:54 +0000

    change to better name

--- WeatherRoutingDatabase/Schema-Model/dbo/Tables/LatLngCoord.sql
+++ WeatherRoutingDatabase/Schema-Model/dbo/Tables/LatLngCoord.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[LatLngCoord]
 (
 [LatLngCoordId] [int] NOT NULL IDENTITY(1, 1),
-[latitude] [varchar] (50) NOT NULL,
-[longitude] [varchar] (50) NOT NULL
+[latitude] [decimal] (8, 5) NOT NULL,
+[longitude] [decimal] (8, 5) NOT NULL
 )
 GO
 ALTER TABLE [dbo].[LatLngCoord] ADD CONSTRAINT [PK_LatLngCoord] PRIMARY KEY CLUSTERED  ([LatLngCoordId])
